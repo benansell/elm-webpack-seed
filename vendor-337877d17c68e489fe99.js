d95ba04b3c75934257f7ce01c7feb35c050807f0
bja <code@benansell.com>
2016-07-20 07:24:52 +0100

    Adds v0.0.1 dist output

---
+++ vendor-337877d17c68e489fe99.js
@@ -0,0 +1,21 @@
+webpackJsonp([2,0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	(function() {
+	    'use strict';
+	
+	    __webpack_require__(3);
+	})();
+
+
+/***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */
+/***/ function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }
+]);
